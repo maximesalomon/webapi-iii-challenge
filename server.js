cf65435e79aeed64048eadb72e412a465006493d
Maxime Salomon <maxime@croissant.io>
2019-03-21 12:51:45 +0100

    fixes for heroku deployment

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const path = require('path');
 const postRouter = require('./routes/post-router')
 const userRouter = require('./routes/user-router')
-
-const pathToBuildFolder = path.join(__dirname, 'build');
 
 const server = express();
 const parser = express.json();
 server.use(parser);
-server.use(express.static(pathToBuildFolder));
 
-server.get('/', (req, res) => {
-  res.send(`<h1>Maxime Salomon - Lambda Blog API</h1>`);
-});
 
 server.use('/api/posts', postRouter)
 server.use('/api/users', userRouter)
