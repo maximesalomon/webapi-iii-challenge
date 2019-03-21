337453457c2d9028884d3704652b7293049c81f1
Maxime Salomon <maxime@croissant.io>
2019-03-21 12:41:33 +0100

    added server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// code away!
-
 const server = require('./server.js');
 
-server.listen(7000, () => {
-  console.log('\n* Server Running on http://localhost:7000 *\n');
+const PORT = process.env.PORT || 7000;
+
+server.listen(PORT, () => {
+  console.log(`\n* Server Running on http://localhost:${PORT} *\n`);
 });
